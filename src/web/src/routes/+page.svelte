8ecdd8668f3b19a4b7be85f0cad8161d353fabf6
aidan-neel <aaidan5899@gmail.com>
2025-05-21 16:20:17 -0500

    fix docker issues

--- src/web/src/routes/+page.svelte
+++ src/web/src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import MapMarker from "$components/business/BusinessMarker.svelte";
     import { generateFakeBusiness } from "$types/Business";
-    import * as FloatingPanel from "$components/ui/floating-panel";
 
     let business = generateFakeBusiness(-86.75, -86.45, 34.65, 34.85); // HSV
 </script>
